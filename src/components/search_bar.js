0ede2de2c11735c4ecfe03811e719b2d540718a3
John Lyden <johnlydennc@gmail.com>
2017-12-19 20:52:04 -0600

    removed unused cdn from index - added onChange and onSubmit callbacks for search input submit

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  
   constructor(props) {
     super(props);
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
+
+  onInputChange(event) {
+		this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+		event.preventDefault();
+    this.setState({ term: '' });
+    // call an action creator to get the list of cars
+    // this.props.fetchCars(this.state.term);
+	}
 
   render() {
     return (
-      <div>
-        <h2>hey</h2>
-      </div>
-    );
+			<div>
+				<form onSubmit={this.onFormSubmit}>
+					<input
+						placeholder="Search for a car"
+						value={this.state.term}
+						onChange={this.onInputChange} 
+          />
+					<span className="">
+						<button type="submit">Search</button>
+					</span>
+				</form>
+			</div>
+		);
   }
 }
+
