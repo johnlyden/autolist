65166530a58c61b84fca7dd972668161aee15eee
John Lyden <johnlydennc@gmail.com>
2017-12-19 18:59:37 -0600

    added a SearchBar component - imported into App and rendering

---
+++ src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>hey</h2>
+      </div>
+    );
+  }
+}
