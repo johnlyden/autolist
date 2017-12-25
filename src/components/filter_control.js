1e514941db94d0fffaebf88300487456cd37dfa8
John Lyden <johnlydennc@gmail.com>
2017-12-25 17:02:53 -0600

    updated css transitions to be a bit more smooth

--- src/components/filter_control.js
+++ src/components/filter_control.js
@@ -1,17 +1,49 @@
 import React, {Component} from 'react';
+import Input from 'material-ui/Input';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button'
 
 export default class FilterControl extends Component {
-  constructor(props) {
-    super(props)
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      min: null,
+      max: null    
+    }
+  }
+
+  handleOnChangeMin = (e) => {
+  
+    this.setState({
+      min: e.target.value
+    })
+  }
+  handleOnChangeMax = (e) => {
+    this.setState({
+      max: e.target.value
+    })
   }
 
   render() {
-    const style = {
-      'width': '100%',
-      'backgroundColor': 'red'
-    }
+    // if (!this.state.show) { return <div></div> };
+    let { min, max } = this.state
     return (
-      <div style={style}>hey</div>
+      <form className="container">
+        <Input
+            id="min"
+						placeholder="Min"
+						value={this.state.min}
+						onChange={this.handleOnChangeMin}
+            className="" 
+          />
+        <Input
+          placeholder="Max"
+          value={this.state.max}
+          onChange={this.handleOnChangeMax}
+          className="" 
+        />
+        <Button type="submit" className="btn btn-primary">Filter by price</Button>
+      </form>
     )
   }
 }
