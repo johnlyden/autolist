f473e86a92c86a60e8b88cbc23afcedfaeabbd84
John Lyden <johnlydennc@gmail.com>
2017-12-25 17:22:25 -0600

    added filter functionality using text inputs

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_CARS = 'FETCH_CARS';
 export const SELECT_CAR = 'SELECT_CAR';
+export const FILTER_PRICE = 'FILTER_PRICE';
 
 const ROOT_URL = 'https://autolist-test.herokuapp.com/search?&'
 
@@ -23,5 +24,16 @@ export function selectCar(carId) {
     return {
         type: SELECT_CAR,
         payload: carId
+    }
+}
+
+export function filterPrice(min, max) {
+    const filterURL = `${ROOT_URL}price_min=${min}&price_max=${max}`;
+    const request = axios.get(filterURL).catch((err) => {
+        console.log('error with the request ', err);
+    });
+    return {
+        type: FETCH_CARS,
+        payload: request
     }
 }
--- src/components/filter_control.js
+++ src/components/filter_control.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { filterPrice } from '../actions/index';
 import Input from 'material-ui/Input';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button'
 
-export default class FilterControl extends Component {
+class FilterControl extends Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
       min: null,
       max: null    
     }
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    
   }
 
   handleOnChangeMin = (e) => {
-  
     this.setState({
       min: e.target.value
     })
@@ -24,11 +28,18 @@ export default class FilterControl extends Component {
     })
   }
 
+  onFormSubmit(e) {
+    e.preventDefault();
+    const min = this.state.min;
+    const max = this.state.max;
+    this.props.filterPrice(min, max);
+  }
+
   render() {
     // if (!this.state.show) { return <div></div> };
     let { min, max } = this.state
     return (
-      <form className="container">
+      <form className="container" onSubmit={this.onFormSubmit}>
         <Input
             id="min"
 						placeholder="Min"
@@ -37,6 +48,7 @@ export default class FilterControl extends Component {
             className="" 
           />
         <Input
+          id="max"
           placeholder="Max"
           value={this.state.max}
           onChange={this.handleOnChangeMax}
@@ -47,3 +59,9 @@ export default class FilterControl extends Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ filterPrice }, dispatch );
+}
+
+export default connect(null, mapDispatchToProps)(FilterControl);
